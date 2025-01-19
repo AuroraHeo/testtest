2bc968e1d8c15dc0e5265a37e19f4b89b7784410
yujinHeo <youjin2586@gmail.com>
2025-01-19 18:44:42 +0900

    second commit

--- test/src/main/java/test/test.java
+++ test/src/main/java/test/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("첫번째 테스트");
 		System.out.println("코드 다시 추가해서 push해보자");
+		System.out.println("왜 안되지?");
 	}
 
 }
