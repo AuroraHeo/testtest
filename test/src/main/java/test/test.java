861d0d305e82844ef9f33d603dbc83744956c1b3
yujinHeo <youjin2586@gmail.com>
2025-01-19 18:38:12 +0900

    Normalize all line endings

--- test/src/main/java/test/test.java
+++ test/src/main/java/test/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("첫번째 테스트");
-		System.out.println("코드 다시 추가했는데");
+		System.out.println("코드 다시 추가해서 push해보자");
 	}
 
 }
